b7f2296fa554a661ec0c9eb292a5951182a27842
iiis9iii <hisham1000020000@gmail.com>
2026-01-04 15:10:16 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+function toggleTheme() {
+  document.body.classList.toggle("light");
+
+  const isLight = document.body.classList.contains("light");
+  localStorage.setItem("theme", isLight ? "light" : "dark");
+}
+
+/* تحميل الوضع المحفوظ */
+window.onload = () => {
+  const savedTheme = localStorage.getItem("theme");
+  if (savedTheme === "light") {
+    document.body.classList.add("light");
+  }
+};
